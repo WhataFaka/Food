c6f5f3cef82537865d3fd4bf42468287eaf79062
Your Name <mrmofd@gmail.com>
2022-02-02 23:38:48 +0700

    better

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,3 +1,5 @@
+import {getResource} from '../services/services';
+
 function cards() {
 
     class MenuCard {
@@ -42,16 +44,6 @@ function cards() {
         }
     }
 
-    const getResource = async (url) => {
-        const res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
-    };
-
     axios.get('http://localhost:3000/menu')
         .then(data => {
             data.data.forEach(({
@@ -66,4 +58,4 @@ function cards() {
         });
 }
 
-module.exports = cards;
+export default cards;
--- js/modules/forms.js
+++ js/modules/forms.js
@@ -1,6 +1,9 @@
-function forms() {
+import {closeModal, openModal} from './modal';
+import {postData} from '../../js/services/services';
 
-    const forms = document.querySelectorAll('form');
+function forms(formSelector, modalTimerId) {
+
+    const forms = document.querySelectorAll(formSelector);
 
     const message = {
         loading: 'img/form/spinner.svg  ',
@@ -11,18 +14,6 @@ function forms() {
     forms.forEach(item => {
         bindPostData(item);
     });
-
-    const postData = async (url, data) => {
-        const res = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: data
-        });
-
-        return await res.json();
-    };
 
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
@@ -59,7 +50,7 @@ function forms() {
         const prevModalDialog = document.querySelector('.modal__dialog');
 
         prevModalDialog.classList.add('hide');
-        openModal();
+        openModal('.modal', modalTimerId);
 
         const thanksModal = document.createElement('div');
         thanksModal.classList.add('modal__dialog');
@@ -74,7 +65,7 @@ function forms() {
             thanksModal.remove();
             prevModalDialog.classList.add('show');
             prevModalDialog.classList.remove('hide');
-            closeModal();
+            closeModal('.modal');
         }, 4000);
     }
 
@@ -83,4 +74,4 @@ function forms() {
         .then(res => console.log(res));
 }
 
-module.exports = forms;
+export default forms;
